38b356c461f2206ca4bc72fa33de17e48a909c73
ElvisNogueira <41121143+ElvisNogueira@users.noreply.github.com>
2019-10-25 01:10:40 -0300

    Fazendo modulo fios

--- BooleanDefuse/src/model/ModuloFios.java
+++ BooleanDefuse/src/model/ModuloFios.java
@@ -5,6 +5,11 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import dao.FiosDao;
+
 /**
  *
  * @author Elvis Nogueira
@@ -15,6 +20,43 @@ public class ModuloFios {
     private Fios[] fios;
     private Sprite[] sprites;
     
+    public ModuloFios() {
+    	fios = sortearFios();
+    	
+    }
     
+    private Fios[] sortearFios() {
+    	Random numAleatorio = new Random();
+    	ArrayList<Fios> fiosXML = FiosDao.getAll();
+    	fios[0]=fiosXML.get(numAleatorio.nextInt(fiosXML.size()));
+    	int i = 0;
+    	int sorteado;
+    	boolean flag = true;
+    	while(i<3) {
+    		sorteado = numAleatorio.nextInt(fiosXML.size());
+    		if(fios.length==0) {
+    			fios[0]=fiosXML.get(sorteado);
+    			i++;
+    		}else {
+    			for (Fios fio : fios) {
+					if(fio==fiosXML.get(sorteado))
+						flag = false;
+				}
+    			if(flag) {
+    				fios[i]=fiosXML.get(sorteado);
+    				i++;
+    			}else
+    				flag = true;
+    		}
+    	}
+    	
+    	return fios;
+    }
+    
+    private Sprite[] setarSprites(Fios[] vetorFios) {
+    	
+    	
+    	return null;
+    }
     
 }
